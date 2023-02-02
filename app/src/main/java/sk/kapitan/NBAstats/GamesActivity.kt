d565d47d941510563353cadaf0824e6eed36393a
nitram154 <husqarna15>
2023-02-02 18:28:42 +0100

    games screen - changes2

--- app/src/main/java/sk/kapitan/NBAstats/GamesActivity.kt
+++ app/src/main/java/sk/kapitan/NBAstats/GamesActivity.kt
@@ -1,19 +1,29 @@
 package sk.kapitan.NBAstats
 
+import android.icu.text.SimpleDateFormat
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.koin.core.parameter.parametersOf
 import sk.kapitan.NBAstats.ui.theme.NBAstatsTheme
+import java.text.ParseException
+import java.time.Instant
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 const val GAMES_KEY = "GAMES_KEY"
 
@@ -30,20 +40,50 @@ class GamesActivity : ComponentActivity() {
                     color = MaterialTheme.colors.background
                 ) {
                     val team = viewModel.team.observeAsState()
+                    val games = viewModel.games.observeAsState()
                     Scaffold(
                         topBar = {
                             TopAppBar { 
-                                IconButton(onClick = { /*TODO*/ }) {
+                                IconButton(onClick = { finish() }) {
                                     Icon(painter = painterResource(id = R.drawable.ic_arrow_back), contentDescription = null)
                                 }
                                 Text(text = team.value?.name ?: "")
                             }
                         }
                     ) {
-                        // Screen content
+                        LazyColumn(content = {
+                            itemsIndexed(games.value?: emptyList()){  index, item ->
+                                Column() {
+                                    Row(modifier = Modifier
+                                        .padding(16.dp)
+                                        .fillMaxWidth()){
+
+                                        Text(text = "${parseDate(item.date) } - ${item.homeTeam.name}  ${item.homeTeamScore} : ${item.visitorTeamScore} ${item.visitorTeam.name}")
+
+                                    }
+                                    if (index < games.value?.lastIndex ?: 0)
+                                        Divider(color = Color.Black, thickness = 1.dp)
+                                }
+                            }
+                        })
                     }
                 }
             }
         }
+    }
+    fun parseDate(
+        inputDateString: String?,
+        inputDateFormat: SimpleDateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.sss'Z'", Locale.US),
+        outputDateFormat: SimpleDateFormat = SimpleDateFormat("dd.MM.yyyy", Locale.US)
+    ): String? {
+        var date: Date? = null
+        var outputDateString: String? = null
+        try {
+            date = inputDateFormat.parse(inputDateString)
+            outputDateString = outputDateFormat.format(date)
+        } catch (e: ParseException) {
+            e.printStackTrace()
+        }
+        return outputDateString
     }
 }
